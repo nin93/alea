9b7ebd075e32ed19a6b8f621013ab440aec151b7
Elia Franzella <eliafranzella@live.it>
2020-04-27 04:24:26 +0200

    Written some docs

--- src/alea/xsr.cr
+++ src/alea/xsr.cr
@@ -1,22 +1,22 @@
 require "random/secure"
 
 module Alea
-  # `XSR` is a replacement for the default `Random` stdlib module that implements
-  # the **xoshiro++** algotithms for generating pseudo-random `UInt`s in 64-bits.
+  # `XSR` is a replacement for the default `Random::PCG32` stdlib prng that implements
+  # the **xoshiro** algotithms for generating pseudo-random `UInt`s in 64-bits.
   # Performance is comparable, but quality of generated `Float`s is much better.
   abstract class XSR
-    # Must return an uniformly-distributed `UInt64`
+    # Must return an uniformly-distributed `UInt64`.
     abstract def next_u : UInt64
 
-    # Must return an uniformly-distributed `Float64` in [0, 1)
+    # Must return an uniformly-distributed `Float64` in [0, 1).
     abstract def next_f : Float64
 
-    # Must perform a jump
-    abstract def jump(delta : UInt64)
+    # Must perform a jump.
+    abstract def jump : self
 
     # :nodoc:
     # http://prng.di.unimi.it/splitmix64.c
-    # This is used as a state initializer
+    # This is used as a state initializer.
     protected def init_state(state, storage, seed)
       storage.times do |i|
         state.value[i] = (seed &+ (i + 1) &* 0x9e3779b97f4a7c15)
@@ -38,27 +38,27 @@ module Alea
   # If more state is needed, check `Alea::XSR256`.
   #
   # ```text
-  #  - period: 2^64 -1
+  #  - period:     2^128 -1
   #  - state type: UInt64
   # ```
   class XSR128 < XSR
     STATE_STORAGE = 2
 
-    # The state of this PRNG
+    # The state of this PRNG.
     @state : StaticArray(UInt64, STATE_STORAGE)
 
-    # Instanciate the PRNG with initial state
+    # Initializes the PRNG with initial state.
     def initialize(initstate : UInt64)
       @state = StaticArray[0u64, 0u64]
       init_state(pointerof(@state), STATE_STORAGE, initstate)
     end
 
-    # Instanciate the PRNG with pseudo-random initial state readed from system resorces
+    # Initializes the PRNG with initial state readed from system resorces.
     def self.new
       new ::Random::Secure.next_u.to_u64
     end
 
-    # Generate a uniformly-distributed random `UInt64`
+    # Generate a uniformly-distributed random `UInt64`.
     #
     # ```
     # rng = Alea::XSR128.new
@@ -75,7 +75,7 @@ module Alea
       rnd
     end
 
-    # Generate a uniformly-distributed random `Float64` in [0, 1)
+    # Generate a uniformly-distributed random `Float64` in [0, 1).
     #
     # ```
     # rng = Alea::XSR128.new
@@ -83,12 +83,14 @@ module Alea
     # ```
     @[AlwaysInline]
     def next_f : Float64
-      # 1.1102230246251565e-16 is ldexp(1.0, -53.0)
+      # 1.1102230246251565e-16 is ldexp(1.0, -53.0).
       (next_u >> 11) * 1.1102230246251565e-16
     end
 
     # TODO:
-    def jump(delta : UInt64)
+    def jump : self
+      raise NotImplementedError.new
+      self
     end
   end
 
@@ -96,33 +98,32 @@ module Alea
   # As a result of this, double steps are needed and therefore performance is affected.
   #
   # ```text
-  #  - period:     2^128
+  #  - period:     2^256 -1
   #  - state type: UInt64
   # ```
   class XSR256 < XSR
-    JUMP = StaticArray[
-      0x180ec6d33cfd0aba_u64, 0xd5a61266f0c9392c_u64,
-      0xa9582618e03fc9aa_u64, 0x39abdc4529b1661c_u64
-    ]
-
     STATE_STORAGE = 4
 
-    # The state of this PRNG
+    JUMP = StaticArray[
+      0x180ec6d33cfd0aba_u64, 0xd5a61266f0c9392c_u64,
+      0xa9582618e03fc9aa_u64, 0x39abdc4529b1661c_u64,
+    ]
+
+    # The state of this PRNG.
     @state : StaticArray(UInt64, STATE_STORAGE)
 
-    # Instanciate the PRNG with initial state
+    # Initializes the PRNG with initial state.
     def initialize(initstate : UInt64)
       @state = StaticArray[0u64, 0u64, 0u64, 0u64]
       init_state(pointerof(@state), STATE_STORAGE, initstate)
     end
 
-    # Instanciate the PRNG with pseudo-random initial state readed
-    # from system resorces. See `Random::Secure`.
+    # Initializes the PRNG with initial state readed from system resorces.
     def self.new
       new ::Random::Secure.next_u.to_u64
     end
 
-    # Generate a uniformly-distributed random `UInt64`
+    # Generate a uniformly-distributed random `UInt64`.
     #
     # ```
     # rng = Alea::XSR256.new
@@ -135,7 +136,7 @@ module Alea
       rnd
     end
 
-    # Generate a uniformly-distributed random `Float64` in [0, 1)
+    # Generate a uniformly-distributed random `Float64` in [0, 1).
     #
     # ```
     # rng = Alea::XSR256.new
@@ -145,14 +146,17 @@ module Alea
     def next_f : Float64
       rnd = @state[0] &+ @state[3]
       xsr_new_state
-      # 1.1102230246251565e-16 is ldexp(1.0, -53.0)
+      # 1.1102230246251565e-16 is ldexp(1.0, -53.0).
       (rnd >> 11) * 1.1102230246251565e-16
     end
 
     # TODO:
-    def jump(delta : UInt64)
+    def jump : self
+      raise NotImplementedError.new
+      self
     end
 
+    # :nodoc:
     @[AlwaysInline]
     protected def xsr_new_state
       tmp = @state[1] << 17
