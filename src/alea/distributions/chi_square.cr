1ee8bfa0ee9c39db22d4c74466b50a8db08a208b
Elia Franzella <eliafranzella@hotmail.it>
2020-04-28 22:53:23 +0200

    Implemented safe methods

--- src/alea/distributions/chi_square.cr
+++ src/alea/distributions/chi_square.cr
@@ -3,6 +3,16 @@ require "./gamma"
 
 module Alea
   class Random
+    # Generate a chi^2-distributed random `Float64` with given degrees of freedom.
+    # Raises ArgumentError if parameter is negative or zero.
+    def chi_square(freedom)
+      if freedom <= 0.0
+        raise ArgumentError.new "Expected freedom parameter to be greater than 0.0."
+      end
+
+      next_chi_square freedom
+    end
+
     # This are written to allow any combination of
     # argument types and avoid tedious manual casting.
     {% for t1 in ["Int".id, "Float".id] %}
