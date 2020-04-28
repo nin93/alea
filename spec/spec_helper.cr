d1de23581d2305e1f3dd4ddbc8c8122dcdeca7b3
Elia Franzella <eliafranzella@live.it>
2020-04-28 23:15:12 +0200

    Moved xsr specs into xsr_spec.cr

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,3 +4,11 @@ require "../src/alea"
 # default pseudo-random number generator for specs
 SpecRng   = Alea::Random.new 9377
 SpecNdata = 5_000_000
+
+def stdev(ary, mean, n)
+  ans = 0.0
+  ary.each do |e|
+    ans += (e - mean) ** 2
+  end
+  Math.sqrt(ans / (n - 1))
+end
