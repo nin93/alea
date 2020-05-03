f3c62c087d2eab4eeb11868e13eb508d95365f1b
Elia Franzella <eliafranzella@hotmail.it>
2020-05-03 17:12:57 +0200

    Created `CDF` module

---
+++ src/alea/cdf.cr
@@ -0,0 +1,8 @@
+module Alea
+  # The CDF module implements the *Cumulative Distribution Functions* for the supported distributions.
+  # The cdf of a real valued random variable X evaluated at x is the probability that X will take a value
+  # less or equal to x. It is the inverse of the *Percent Point Function* (PPF), the one used by most of
+  # the implementations inside this library to perform sampling.
+  module CDF
+  end
+end
