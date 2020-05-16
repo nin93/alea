988590a8f350b843205d6c01ced1d00105912bf0
Elia Franzella <eliafranzella@live.it>
2020-05-16 23:05:01 +0200

    MAINT: moved `VERSION` to toplevel

--- src/alea.cr
+++ src/alea.cr
@@ -9,4 +9,5 @@ require "./alea/*"
 # Algorithms in this library are heavily derived from [NumPy](https://github.com/numpy/numpy) and
 # [Julia](https://github.com/JuliaLang/julia) lang. Disclaimer in LICENSE file.
 module Alea
+  VERSION = "0.2.3"
 end
