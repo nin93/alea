9836e30a499485ccf2654e2f75f1c6997acd14eb
Elia Franzella <eliafranzella@live.it>
2020-05-16 22:53:22 +0200

    REF: moved `CDF` methods into different files

--- src/alea/rand/lognormal.cr
+++ src/alea/rand/lognormal.cr
@@ -42,16 +42,4 @@ module Alea
       {% end %}
     {% end %}
   end
-
-  module CDF
-    # Returns the probability of X being less or equal to x
-    # with given mean and standard deviation of the underlying normal distribution.
-    def lognormal(x : Float, mean : Float = 0.0, sigma : Float = 1.0) : Float64
-      unless sigma > 0.0
-        raise ArgumentError.new "Expected sigma to be positive."
-      end
-      x <= 0.0 && return 0.0
-      0.5 + 0.5 * (Math.erf((Math.log(x) - mean) / (sigma * 1.4142135623730951)))
-    end
-  end
 end
--- src/alea/rand/uniform.cr
+++ src/alea/rand/uniform.cr
@@ -66,17 +66,4 @@ module Alea
       @prng.next_f * span + range.begin
     end
   end
-
-  module CDF
-    # Returns the probability of X being less or equal to x
-    # with given scale of the distribution.
-    def uniform(x : Float, min : Float, max : Float) : Float64
-      unless min < max
-        raise ArgumentError.new "Invalid range for uniform: #{min}...#{max}"
-      end
-      x <= min && return 0.0
-      x >= max && return 1.0
-      (x - min) / (max - min)
-    end
-  end
 end
