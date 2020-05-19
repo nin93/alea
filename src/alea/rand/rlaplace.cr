ce3e0580f8eeb0998cb4181c30caaac9d64d2d47
Elia Franzella <eliafranzella@live.it>
2020-05-20 01:18:46 +0200

    DOC: refined documentation for `laplace`

--- src/alea/rand/rlaplace.cr
+++ src/alea/rand/rlaplace.cr
@@ -2,9 +2,16 @@ require "../internal/ierr"
 
 module Alea
   struct Random
-    # Generate a laplace-distributed random `Float64`
-    # with given center and scale.
-    # Raises ArgumentError if scale parameter is negative or zero.
+    # Generate a *laplace-distributed*, pseudo-random `Float64`.
+    #
+    # Parameters:
+    # - **mean**: centrality parameter, or mean of the distribution
+    # - **scale**: scale parameter of the distribution
+    #
+    # Raises:
+    # - `Alea::NaNError` if any of the arguments is NaN
+    # - `Alea::InfinityError` if any of the arguments is Infinity
+    # - `Alea::UndefinedError` if **scale** is negative or zero
     def laplace(mean = 0.0, scale = 1.0)
       Alea.sanity_check(mean, :mean, :laplace)
       Alea.sanity_check(scale, :scale, :laplace)
@@ -12,8 +19,9 @@ module Alea
       next_laplace mean, scale
     end
 
-    # Generate a standard laplace-distributed random `Float64`
-    # centred in 0.0 and scaled by 1.0.
+    # :nodoc:
+    # Unwrapped version of `laplace`.
+    # Generate a *laplace-distributed*, pseudo-random `Float64`.
     def next_laplace : Float64
       while true
         u = @prng.next_f
@@ -25,8 +33,9 @@ module Alea
     # This are written to allow any combination of
     # argument types and avoid tedious manual casting.
     {% for t1 in ["Int".id, "Float".id] %}
-      # Generate a standard laplace-distributed random `Float64`
-      # centred in 0.0 and scaled by 1.0.
+      # :nodoc:
+      # Unwrapped version of `laplace`.
+      # Generate a *laplace-distributed*, pseudo-random `Float64`.
       def next_laplace(mean : {{t1}}) : Float64
         while true
           u = @prng.next_f
@@ -40,8 +49,9 @@ module Alea
     # argument types and avoid tedious manual casting.
     {% for t1 in ["Int".id, "Float".id] %}
       {% for t2 in ["Int".id, "Float".id] %}
-        # Generate a laplace-distributed random `Float64`
-        # with given center and scale.
+        # :nodoc:
+        # Unwrapped version of `laplace`.
+        # Generate a *laplace-distributed*, pseudo-random `Float64`.
         def next_laplace(mean : {{t1}}, scale : {{t2}}) : Float64
           while true
             u = @prng.next_f
