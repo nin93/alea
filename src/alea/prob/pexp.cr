c91809a8be098eed5f30c8a79c913755f69bce78
Elia Franzella <eliafranzella@hotmail.it>
2020-05-19 23:47:36 +0200

    ENH: added Exception interface to CDF methods

--- src/alea/prob/pexp.cr
+++ src/alea/prob/pexp.cr
@@ -2,9 +2,9 @@ module Alea::CDF
   # Returns the probability of X being less or equal to x
   # with given scale of the distribution. Scale parameter is lambda^-1.
   def self.exponential(x : Float, scale : Float = 1.0) : Float64
-    unless scale > 0.0
-      raise ArgumentError.new "Expected scale to be positive."
-    end
+    Alea.sanity_check(x, :x, :exponential)
+    Alea.sanity_check(scale, :scale, :exponential)
+    Alea.param_check(scale, :<=, 0.0, :scale, :exponential)
     x <= 0.0 && return 0.0
     1.0 - Math.exp(-x / scale)
   end
--- src/alea/prob/punif.cr
+++ src/alea/prob/punif.cr
@@ -2,8 +2,11 @@ module Alea::CDF
   # Returns the probability of X being less or equal to x
   # with given scale of the distribution.
   def self.uniform(x : Float, min : Float, max : Float) : Float64
+    Alea.sanity_check(x, :x, :uniform)
+    Alea.sanity_check(min, :min, :uniform)
+    Alea.sanity_check(max, :max, :uniform)
     unless min < max
-      raise ArgumentError.new "Invalid range for uniform: #{min}...#{max}"
+      raise Alea::UndefinedError.new "Invalid value for `uniform': range = #{min}...#{max}"
     end
     x <= min && return 0.0
     x >= max && return 1.0
