27824742c898701c5e6be26233400c55637f02d3
Elia Franzella <eliafranzella@live.it>
2020-05-20 02:13:51 +0200

    DOC: refined documentation for `chi_square`

--- src/alea/rand/rchisq.cr
+++ src/alea/rand/rchisq.cr
@@ -2,8 +2,15 @@ require "./rgamma"
 
 module Alea
   struct Random
-    # Generate a chi^2-distributed random `Float64` with given degrees of freedom.
-    # Raises ArgumentError if parameter is negative or zero.
+    # Generate a *chi-square-distributed*, pseudo-random `Float64`.
+    #
+    # Parameters:
+    # - **freedom**: degrees of freedom parameter of the distribution
+    #
+    # Raises:
+    # - `Alea::NaNError` if any of the arguments is NaN
+    # - `Alea::InfinityError` if any of the arguments is Infinity
+    # - `Alea::UndefinedError` if **freedom** is negative or zero
     def chi_square(freedom)
       Alea.sanity_check(freedom, :freedom, :chi_square)
       Alea.param_check(freedom, :<=, 0.0, :freedom, :chi_square)
@@ -13,8 +20,9 @@ module Alea
     # This are written to allow any combination of
     # argument types and avoid tedious manual casting.
     {% for t1 in ["Int".id, "Float".id] %}
-      # Generate a chi^2-distributed random `Float64`
-      # with given degrees of freedom.
+      # :nodoc:
+      # Unwrapped version of `chi_square`.
+      # Generate a *chi-square-distributed*, pseudo-random `Float64`.
       def next_chi_square(freedom : {{t1}}) : Float64
         next_gamma(freedom / 2.0) * 2.0
       end
