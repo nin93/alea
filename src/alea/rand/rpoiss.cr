caa0508c6bf892ed32247bf0a7e95b23481d8f83
Elia Franzella <eliafranzella@live.it>
2020-05-20 03:17:08 +0200

    DOC: refined documentation for `poisson`

--- src/alea/rand/rpoiss.cr
+++ src/alea/rand/rpoiss.cr
@@ -2,15 +2,24 @@ require "../internal/ipoiss"
 
 module Alea
   struct Random
-    # Generate a poisson-distributed random `Int64` with given lambda parameter.
-    # Raises ArgumentError if lambda parameter is negative or zero.
+    # Generate a *poisson-distributed*, pseudo-random `Int64`.
+    #
+    # Parameters:
+    # - **lam**: separation parameter of the distribution, also mentioned as *lambda*
+    #
+    # Raises:
+    # - `Alea::NaNError` if any of the arguments is NaN
+    # - `Alea::InfinityError` if any of the arguments is Infinity
+    # - `Alea::UndefinedError` if **lam** is negative or zero
     def poisson(lam = 1.0)
       Alea.sanity_check(lam, :lam, :poisson)
       Alea.param_check(lam, :<=, 0.0, :lam, :poisson)
       next_poisson lam
     end
 
-    # Generate a standard poisson-distributed random `Int64` with lambda 1.0.
+    # :nodoc:
+    # Unwrapped version of `poisson`.
+    # Generate a *poisson-distributed*, pseudo-random `Int64`.
     def next_poisson : Int64
       Alea::Internal.poisson_mult 1.0, @prng
     end
@@ -18,8 +27,9 @@ module Alea
     # This are written to allow any combination of
     # argument types and avoid tedious manual casting.
     {% for t1 in ["Int".id, "Float".id] %}
-      # Generate a poisson-distributed random `Int64`
-      # with given lambda parameter.
+      # :nodoc:
+      # Unwrapped version of `poisson`.
+      # Generate a *poisson-distributed*, pseudo-random `Int64`.
       def next_poisson(lam : {{t1}}) : Int64
         (lam >= 10.0) && return Alea::Internal.poisson_ptrs lam, @prng
         (lam == 0.0) && return 0i64
