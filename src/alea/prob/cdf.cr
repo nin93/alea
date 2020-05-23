fba1dfb06cc43ded468a557446abbf4d4c9b027f
Elia Franzella <eliafranzella@live.it>
2020-05-23 16:43:44 +0200

    DOC: Added better docs to `CDF` module

--- src/alea/prob/cdf.cr
+++ src/alea/prob/cdf.cr
@@ -1,8 +1,18 @@
 module Alea
-  # The CDF module implements the *Cumulative Distribution Functions* for the supported distributions.
-  # The cdf of a real valued random variable X evaluated at x is the probability that X will take a value
-  # less or equal to x. It is the inverse of the *Percent Point Function* (PPF), the one used by most of
-  # the implementations inside this library to perform sampling.
+  # `Alea::CDF` is the interface used to calculate the Cumulative Distribution Functions.
+  # Given *X* ~ *D* and a fixed quantile *x*, CDFs are defined as the functions that associate *x*
+  # to the probability that the real-valued random *X* from the distribution *D*
+  # will take a value less or equal to *x*.
+  #
+  # Arguments passed to `Alea::CDF` methods to shape the distributions are analogous to those used for sampling:
+  #
+  # ```crystal
+  # Alea::CDF.normal(0.0)                       # => 0.5
+  # Alea::CDF.normal(2.0, loc: 1.0, sigma: 0.5) # => 0.9772498680518208
+  # Alea::CDF.chisq(5.279, df: 5.0)             # => 0.6172121213841358
+  # ```
+  #
+  # NOTE: for real-valued quantiles is used `x`, `k` for discrete-valued instead.
   module CDF
   end
 end
