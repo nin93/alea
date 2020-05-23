9be9cd8a3283693f132e63e59f3494b864bf3a12
Elia Franzella <eliafranzella@live.it>
2020-05-23 18:29:16 +0200

    ENH: Added cdf support for Poisson distribution

---
+++ src/alea/prob/ppoiss.cr
@@ -0,0 +1,20 @@
+module Alea::CDF
+  # Calculate the cumulative distribution function evaluated at `k`.
+  #
+  # **@parameters**:
+  # * `k`: discrete-valued quantile of which estimate the cdf.
+  # * `lam`: separation parameter of the distribution;
+  #   usually mentioned as **`λ`**.
+  #
+  # **@exceptions**:
+  # * `Alea::NaNError` if any of the arguments is `NaN`.
+  # * `Alea::InfinityError` if any of the arguments is `Infinity`.
+  # * `Alea::UndefinedError` if `lam` is negative or zero.
+  def self.poisson(k : Int, lam : Float = 1.0) : Float64
+    # No sanity check here: k is an Int
+    Alea.sanity_check(lam, :lam, :poisson)
+    Alea.param_check(lam, :<=, 0.0, :lam, :poisson)
+    k < 0 && return 0.0
+    Alea::Internal.incg_regular_upper(k + 1, lam)
+  end
+end
