eb7d75d5527d65a86b252ccbbc191c63aa77acdf
Elia Franzella <eliafranzella@live.it>
2020-05-23 22:13:19 +0200

    MAINT: Renamed `comp` parameter to `op` in `param_check` macro

--- src/alea/internal/ierr.cr
+++ src/alea/internal/ierr.cr
@@ -32,19 +32,19 @@ module Alea
   # Compile-time expansion for dist. definition checks over arguments.
   #
   # **@parameters**:
-  # * `{{x}}`: parameter to check through `{{comp}}` with `{{y}}`.
-  # * `{{comp}}`: operator that performs the comparison.
-  # * `{{y}}`: parameter to check through `{{comp}}` with `{{x}}`.
+  # * `{{x}}`: parameter to check through `{{op}}` with `{{y}}`.
+  # * `{{op}}`: operator that performs the comparison.
+  # * `{{y}}`: parameter to check through `{{op}}` with `{{x}}`.
   # * `{{param}}`: name of the parameter to display in exception messages.
   # * `{{caller}}`: name of the method to display in exception messages.
   #
   # **@exceptions**:
-  # * `Alea::UndefinedError` if `{{x}}` `{{comp}}` `{{y}}` returns `true`.
-  macro param_check(x, comp, y, param, caller)
-    if {{x.id}} {{comp.id}} {{y.id}}
+  # * `Alea::UndefinedError` if `{{x}}` `{{op}}` `{{y}}` returns `true`.
+  macro param_check(x, op, y, param, caller)
+    if {{x.id}} {{op.id}} {{y.id}}
       # Distribution not defined under these conditions
       raise Alea::UndefinedError.new \
-        "Invalid value for `{{caller.id}}': {{param.id}} = #{{{x.id}}} {{comp.id}} #{{{y.id}}}"
+        "Invalid value for `{{caller.id}}': {{param.id}} = #{{{x.id}}} {{op.id}} #{{{y.id}}}"
     end
   end
 end
