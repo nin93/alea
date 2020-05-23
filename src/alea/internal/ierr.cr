f434b0367720be87fc7bac1804ac2a9c5cee32ad
Elia Franzella <eliafranzella@live.it>
2020-05-24 00:41:37 +0200

    DOC: Added info for Exceptions

--- src/alea/internal/ierr.cr
+++ src/alea/internal/ierr.cr
@@ -1,10 +1,14 @@
 module Alea
+  # Exception raised when distribution conditions are not met.
   class UndefinedError < Exception; end
 
+  # Exception raised when `NaN` is passed as argument.
   class NaNError < Exception; end
 
+  # Exception raised when `Infinity` is passed as argument.
   class InfinityError < Exception; end
 
+  # Exception raised when algorithm has not converged within a fixed limit of iterations.
   class NoConvergeError < Exception; end
 
   # Compile-time expansion for sanity (NaN/Infinity) checks over arguments.
