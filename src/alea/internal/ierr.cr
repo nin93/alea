1695f826d1e1faecfacdf7561010ac91a9de8fe3
Elia Franzella <eliafranzella@live.it>
2020-05-23 22:11:32 +0200

    DOC: Added documentation to macros

--- src/alea/internal/ierr.cr
+++ src/alea/internal/ierr.cr
@@ -7,6 +7,16 @@ module Alea
 
   class NoConvergeError < Exception; end
 
+  # Compile-time expansion for sanity (NaN/Infinity) checks over arguments.
+  #
+  # **@parameters**:
+  # * `{{x}}`: parameter to check.
+  # * `{{param}}`: name of the parameter to display in exception messages.
+  # * `{{caller}}`: name of the method to display in exception messages.
+  #
+  # **@exceptions**:
+  # * `Alea::NaNError` if `{{x}}` is `NaN`.
+  # * `Alea::InfinityError` if `{{x}}` is `Infinity`.
   macro sanity_check(x, param, caller)
     if !({{x.id}} == {{x.id}})
       # NaN encountered
@@ -19,8 +29,20 @@ module Alea
     end
   end
 
+  # Compile-time expansion for dist. definition checks over arguments.
+  #
+  # **@parameters**:
+  # * `{{x}}`: parameter to check through `{{comp}}` with `{{y}}`.
+  # * `{{comp}}`: operator that performs the comparison.
+  # * `{{y}}`: parameter to check through `{{comp}}` with `{{x}}`.
+  # * `{{param}}`: name of the parameter to display in exception messages.
+  # * `{{caller}}`: name of the method to display in exception messages.
+  #
+  # **@exceptions**:
+  # * `Alea::UndefinedError` if `{{x}}` `{{comp}}` `{{y}}` returns `true`.
   macro param_check(x, comp, y, param, caller)
     if {{x.id}} {{comp.id}} {{y.id}}
+      # Distribution not defined under these conditions
       raise Alea::UndefinedError.new \
         "Invalid value for `{{caller.id}}': {{param.id}} = #{{{x.id}}} {{comp.id}} #{{{y.id}}}"
     end
