6dd4ac28c1603dc90cfaf594d4027bf770589ad1
Elia Franzella <eliafranzella@live.it>
2020-05-29 02:16:37 +0200

    SPEC: Added more `gamma` sampling specs

--- spec/rand/rgamma_spec.cr
+++ spec/rand/rgamma_spec.cr
@@ -47,6 +47,141 @@ describe Alea do
         # mean  is:   ks
         # stdev is:   sqrt( ks^2 )
 
+        dist_test("generates gamma-distributed random values with fixed shape 0.1 and scale 0.1 parameters",
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 0.1, scale: 0.1},
+          sample_type: Float64,
+          real_mean: 0.01,
+          real_stdev: 0.0316227766016838,
+          mean_tol: 0.005,
+          stdev_tol: 0.05,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 0.0001 and scale 0.0001 parameters",
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 0.0001, scale: 0.0001},
+          sample_type: Float64,
+          real_mean: 0.00000001,
+          real_stdev: 1.0e-6,
+          mean_tol: 0.005,
+          stdev_tol: 0.2,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 0.0000001 and scale 0.0000001 parameters",
+          pending: true,
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 0.0000001, scale: 0.0000001},
+          sample_type: Float64,
+          real_mean: 1.0e-16,
+          real_stdev: 3.162277660168379e-11,
+          mean_tol: 0.005,
+          stdev_tol: 0.2,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 0.001 and scale 0.0000001 parameters",
+          pending: true,
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 0.001, scale: 0.0000001},
+          sample_type: Float64,
+          real_mean: 1.0e-10,
+          real_stdev: 3.162277660168379e-9,
+          mean_tol: 0.005,
+          stdev_tol: 0.05,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 0.0000001 and scale 0.001 parameters",
+          pending: true,
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 0.0000001, scale: 0.001},
+          sample_type: Float64,
+          real_mean: 1.0e-10,
+          real_stdev: 3.162277660168379e-7,
+          mean_tol: 0.005,
+          stdev_tol: 0.05,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 1.0 and scale 1.0 parameters",
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 1.0, scale: 1.0},
+          sample_type: Float64,
+          real_mean: 1.0,
+          real_stdev: 1.0,
+          mean_tol: 0.005,
+          stdev_tol: 0.01,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 1.0 and scale 10.0 parameters",
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 1.0, scale: 10.0},
+          sample_type: Float64,
+          real_mean: 10.0,
+          real_stdev: 10.0,
+          mean_tol: 0.005,
+          stdev_tol: 0.01,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 1.0 and scale 100.0 parameters",
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 1.0, scale: 100.0},
+          sample_type: Float64,
+          real_mean: 100.0,
+          real_stdev: 100.0,
+          mean_tol: 0.005,
+          stdev_tol: 0.01,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 1.0 and scale 10_000.0 parameters",
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 1.0, scale: 10_000.0},
+          sample_type: Float64,
+          real_mean: 10_000.0,
+          real_stdev: 10_000.0,
+          mean_tol: 0.005,
+          stdev_tol: 0.01,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 10.0 and scale 1.0 parameters",
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 10.0, scale: 1.0},
+          sample_type: Float64,
+          real_mean: 10.0,
+          real_stdev: 3.1622776601683795,
+          mean_tol: 0.005,
+          stdev_tol: 0.01,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 100.0 and scale 1.0 parameters",
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 100.0, scale: 1.0},
+          sample_type: Float64,
+          real_mean: 100.0,
+          real_stdev: 10.0,
+          mean_tol: 0.005,
+          stdev_tol: 0.01,
+        )
+
+        dist_test("generates gamma-distributed random values with fixed shape 10_000.0 and scale 1.0 parameters",
+          caller: SpecRng,
+          method: :next_gamma,
+          params: {shape: 10_000.0, scale: 1.0},
+          sample_type: Float64,
+          real_mean: 10_000.0,
+          real_stdev: 100.0,
+          mean_tol: 0.005,
+          stdev_tol: 0.01,
+        )
+
         dist_test("generates gamma-distributed random values with fixed shape 3.0 and scale 1.0 parameters",
           caller: SpecRng,
           method: :next_gamma,
