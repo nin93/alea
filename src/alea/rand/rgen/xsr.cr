33df4b16fcdbf2ab090cb43c41f64dd217f7d04c
Elia Franzella <eliafranzella@live.it>
2020-06-03 04:49:45 +0200

    ENH: `XSR256`: Added 32-bit gen. support

--- src/alea/rand/rgen/xsr.cr
+++ src/alea/rand/rgen/xsr.cr
@@ -1,4 +1,4 @@
-require "random/secure"
+require "random/isaac"
 require "../../core/cgen"
 require "./prng"
 
@@ -192,10 +192,10 @@ module Alea
     protected def xsr32_next_state
       tmp = @state32[1] << 9
       @state32[2] ^= @state32[0]
-    	@state32[3] ^= @state32[1]
-    	@state32[1] ^= @state32[2]
-    	@state32[0] ^= @state32[3]
-    	@state32[2] ^= tmp
+      @state32[3] ^= @state32[1]
+      @state32[1] ^= @state32[2]
+      @state32[0] ^= @state32[3]
+      @state32[2] ^= tmp
       @state32[3] = rotate(@state32[3], 11, size: 32)
     end
 
@@ -243,105 +243,224 @@ module Alea
   #  - state type: UInt64
   # ```
   class XSR256 < Alea::XSR
-    STATE_STORAGE = 4
+    STATE_STORAGE_64 = 4
 
-    JUMP_128 = StaticArray[
-      0x180ec6d33cfd0aba_u64, 0xd5a61266f0c9392c_u64,
-      0xa9582618e03fc9aa_u64, 0x39abdc4529b1661c_u64,
-    ]
+    # The state this PRNG refers to when called for generating `UInt32`s.
+    @state32 : StaticArray(UInt64, STATE_STORAGE_64)
 
-    JUMP_192 = StaticArray[
-      0x76e15d3efefdcbbf_u64, 0xc5004e441c522fb3_u64,
-      0x77710069854ee241_u64, 0x39109bb02acbe635_u64,
-    ]
+    # The state this PRNG refers to when called for generating `UInt64`s.
+    @state64 : StaticArray(UInt64, STATE_STORAGE_64)
 
-    # The state of this PRNG.
-    @state : StaticArray(UInt64, STATE_STORAGE)
+    # Pending pseudo-random value discarded from partial generation to be reused.
+    @pending : UInt32 | Nil
+
+    # Initializes the PRNG with initial seeds.
+    #
+    # **@parameters**:
+    # * `seed32`: value as input to init. the state of 32-bit generators.
+    # * `seed64`: value as input to init. the state of 64-bit generators.
+    #
+    # **@note**: both 32-bit and 64-bit states are internally processed
+    #   as arrays of `UInt64`, and so is the type of seeds.
+    #
+    # **@references**:
+    # * `Alea::Core::SplitMix64(4)#init_state`.
+    def initialize(seed32 : UInt64, seed64 : UInt64)
+      @state32 = Alea::Core::SplitMix64(STATE_STORAGE_64).init_state seed32
+      @state64 = Alea::Core::SplitMix64(STATE_STORAGE_64).init_state seed64
+    end
 
     # Initializes the PRNG with initial seed.
     #
     # **@parameters**:
-    # * `seed`: initial seed as input for generating the state of this PRNG.
+    # * `seed`: value as input to init. the state of both 32-bit and 64-bit generators.
     #
-    # **@references**: `Alea::Core::SplitMix64(4)#init_state`.
-    def initialize(seed : UInt64)
-      @state = Alea::Core::SplitMix64(STATE_STORAGE).init_state(seed)
+    # **@references**:
+    # * `Alea::Core::SplitMix64(4)#init_state`.
+    #
+    # **@exceptions**:
+    # * `ArgumentError` if `seed` is negative.
+    def initialize(seed : Int)
+      if seed < 0
+        raise ArgumentError.new "Invalid value for `XSR256.new`: seed = #{seed}"
+      else
+        @state32 = Alea::Core::SplitMix64(STATE_STORAGE_64).init_state seed.to_u64
+        @state64 = Alea::Core::SplitMix64(STATE_STORAGE_64).init_state seed.to_u64
+      end
     end
 
-    # Initializes the PRNG with initial state.
+    # Initializes the PRNG with initial states.
     #
     # **@parameters**:
-    # * `state`: initial state of this PRNG.
-    def initialize(@state : StaticArray(UInt64, STATE_STORAGE))
+    # * `state32`: array of values for state of 32-bit generators.
+    # * `state64`: array of values for state of 64-bit generators.
+    def initialize(@state32 : StaticArray(UInt64, STATE_STORAGE_64),
+                   @state64 : StaticArray(UInt64, STATE_STORAGE_64))
     end
 
-    # Initializes the PRNG with initial seed readed from system resources.
+    # Initializes the PRNG with initial seeds readed from system resources.
     def self.new
-      new ::Random::Secure.next_u.to_u64
+      # Cryptographically secure PRNG
+      secure = ::Random::ISAAC.new
+      # Merge two 32-bit integers to obtain seed32
+      lt32 = secure.next_u
+      rt32 = secure.next_u
+      seed32 = ((0u64 | lt32) << 32) | rt32
+      # Merge two 32-bit integers to obtain seed64
+      lt32 = secure.next_u
+      rt32 = secure.next_u
+      seed64 = ((0u64 | lt32) << 32) | rt32
+      new seed32, seed64
     end
 
-    # Generate a uniformly-distributed random `UInt64`.
+    # Generate a uniform-distributed random `UInt32`.
     #
+    # **@examples**:
+    # ```
+    # rng = Alea::XSR128.new
+    # rng.next_u32 # => 1767702788
+    # ```
+    @[AlwaysInline]
+    def next_u32 : UInt32
+      if @pending
+        # Previously stored value
+        rnd = @pending.as UInt32
+        xsr32_next_state
+        @pending = nil
+      else
+        # Unpack `UInt64` into two `UInt32`
+        tmp = rotate(@state32[0] &+ @state32[3], 23, size: 64) &+ @state32[0]
+        # Store value for next call
+        @pending = 0u32 | tmp >> 32
+        rnd = 0u32 | tmp
+      end
+      rnd
+    end
+
+    # Generate a uniform-distributed random `UInt64`.
+    #
+    # **@examples**:
     # ```
     # rng = Alea::XSR256.new
     # rng.next_u # => 9136120204379184874
     # ```
     @[AlwaysInline]
-    def next_u : UInt64
-      rnd = rotate(@state[0] &+ @state[3], 23) &+ @state[0]
-      xsr_new_state
+    def next_u64 : UInt64
+      rnd = rotate(@state64[0] &+ @state64[3], 23, size: 64) &+ @state64[0]
+      xsr64_next_state
       rnd
     end
 
-    # Generate a uniformly-distributed random `Float64` in [0, 1).
+    # Generate a uniform-distributed random `Float32` in `[0, 1)`.
     #
+    # **@examples**:
     # ```
     # rng = Alea::XSR256.new
-    # rng.next_f # => 0.12194680409000741
+    # rng.next_f32 # => 0.41157538
     # ```
     @[AlwaysInline]
-    def next_f : Float64
-      rnd = @state[0] &+ @state[3]
-      xsr_new_state
+    def next_f32 : Float32
+      # 5.9604645e-8 is ldexp(1.0, -24.0).
+      (next_u32 >> 8) * 5.9604645e-08f32
+    end
+
+    # Generate a uniform-distributed random `Float64` in `[0, 1)`.
+    #
+    # **@examples**:
+    # ```
+    # rng = Alea::XSR256.new
+    # rng.next_f64 # => 0.12194680409000741
+    # ```
+    @[AlwaysInline]
+    def next_f64 : Float64
       # 1.1102230246251565e-16 is ldexp(1.0, -53.0).
-      (rnd >> 11) * 1.1102230246251565e-16
+      (next_u64 >> 11) * 1.1102230246251565e-16f64
     end
 
-    # This is the equivalent of 2^128 calls to `#next_u`.
-    def jump : self
-      jumper JUMP_128
+    JUMP_64B_128 = StaticArray[
+      0x180ec6d33cfd0aba_u64, 0xd5a61266f0c9392c_u64,
+      0xa9582618e03fc9aa_u64, 0x39abdc4529b1661c_u64,
+    ]
+
+    JUMP_64B_192 = StaticArray[
+      0x76e15d3efefdcbbf_u64, 0xc5004e441c522fb3_u64,
+      0x77710069854ee241_u64, 0x39109bb02acbe635_u64,
+    ]
+
+    # Performs an advance over the 32-bit state.
+    # This is the equivalent of `2^128` calls to `#next_u32`.
+    #
+    # **@parameters**:
+    # * `long`: flag to advance the state by `2^192` calls.
+    def jump_32(long : Bool = false) : self
+      if long
+        xsr32_advance_state JUMP_64B_192
+      else
+        xsr32_advance_state JUMP_64B_128
+      end
       self
     end
 
-    # This is the equivalent of 2^192 calls to `#next_u`.
-    def long_jump : self
-      jumper JUMP_192
+    # Performs an advance over the 32-bit state.
+    # This is the equivalent of `2^128` calls to `#next_u32`.
+    #
+    # **@parameters**:
+    # * `long`: flag to advance the state by `2^192` calls.
+    def jump_64(long : Bool = false) : self
+      if long
+        xsr64_advance_state JUMP_64B_192
+      else
+        xsr64_advance_state JUMP_64B_128
+      end
       self
     end
 
-    private def jumper(const)
-      tmp = StaticArray(UInt64, STATE_STORAGE).new 0u64
-      STATE_STORAGE.times do |i|
+    protected def xsr32_advance_state(const)
+      tmp = StaticArray(UInt64, STATE_STORAGE_64).new 0_u64
+      STATE_STORAGE_64.times do |i|
         64.times do |j|
-          if const[i] & (1u64 << j) != 0
-            STATE_STORAGE.times { |i| tmp[i] ^= @state[i] }
+          if const[i] & (1_u64 << j) != 0
+            STATE_STORAGE_64.times { |k| tmp[k] ^= @state32[k] }
           end
-          next_u
+          xsr32_next_state
         end
       end
-      @state = tmp
+      @state32 = tmp
     end
 
-    # :nodoc:
+    protected def xsr64_advance_state(const)
+      tmp = StaticArray(UInt64, STATE_STORAGE_64).new 0_u64
+      STATE_STORAGE_64.times do |i|
+        64.times do |j|
+          if const[i] & (1_u64 << j) != 0
+            STATE_STORAGE_64.times { |k| tmp[k] ^= @state64[k] }
+          end
+          xsr64_next_state
+        end
+      end
+      @state64 = tmp
+    end
+
     @[AlwaysInline]
-    protected def xsr_new_state
-      tmp = @state[1] << 17
-      @state[2] ^= @state[0]
-      @state[3] ^= @state[1]
-      @state[1] ^= @state[2]
-      @state[0] ^= @state[3]
-      @state[2] ^= tmp
-      @state[3] = rotate(@state[3], 45)
+    protected def xsr32_next_state
+      tmp = @state32[1] << 17
+      @state32[2] ^= @state32[0]
+      @state32[3] ^= @state32[1]
+      @state32[1] ^= @state32[2]
+      @state32[0] ^= @state32[3]
+      @state32[2] ^= tmp
+      @state32[3] = rotate(@state32[3], 45, size: 64)
+    end
+
+    @[AlwaysInline]
+    protected def xsr64_next_state
+      tmp = @state64[1] << 17
+      @state64[2] ^= @state64[0]
+      @state64[3] ^= @state64[1]
+      @state64[1] ^= @state64[2]
+      @state64[0] ^= @state64[3]
+      @state64[2] ^= tmp
+      @state64[3] = rotate(@state64[3], 45, size: 64)
     end
   end
 end
