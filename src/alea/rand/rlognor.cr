2378ff42cef667f3f96e2441428a8e2a9afa4bb8
Elia Franzella <eliafranzella@live.it>
2020-06-05 00:08:26 +0200

    REF,BC: Reshaped modularity in lognormal sampling methods

--- src/alea/rand/rlognor.cr
+++ src/alea/rand/rlognor.cr
@@ -16,41 +16,63 @@ module Alea
     # * `Alea::InfinityError` if any of the arguments is `Infinity`.
     # * `Alea::UndefinedError` if `sigma` is negative or zero.
     def lognormal(loc = 0.0, sigma = 1.0)
-      Alea.sanity_check(loc, :loc, :lognormal)
-      Alea.sanity_check(sigma, :sigma, :lognormal)
+      __lognormal64 loc, sigma
+    end
+
+    # Run-time argument sanitizer for `#lognormal`.
+    private def __lognormal64(loc : Number, sigma : Number) : Float64
       Alea.param_check(sigma, :<=, 0.0, :sigma, :lognormal)
-      next_lognormal loc, sigma
-    end
 
-    # :nodoc:
-    # Unwrapped version of `lognormal`.
-    # Generate a *lognormal-distributed*, pseudo-random `Float64`.
-    def next_lognormal : Float64
-      Math.exp(next_normal)
-    end
-
-    # This are written to allow any combination of
-    # argument types and avoid tedious manual casting.
-    {% for t1 in ["Int".id, "Float".id] %}
-      # :nodoc:
-      # Unwrapped version of `lognormal`.
-      # Generate a *lognormal-distributed*, pseudo-random `Float64`.
-      def next_lognormal(loc : {{t1}}) : Float64
-        Math.exp(next_normal + loc)
+      if loc.class < Float
+        Alea.sanity_check(loc, :loc, :lognormal)
       end
-    {% end %}
 
-    # This are written to allow any combination of
-    # argument types and avoid tedious manual casting.
-    {% for t1 in ["Int".id, "Float".id] %}
-      {% for t2 in ["Int".id, "Float".id] %}
-        # :nodoc:
-        # Unwrapped version of `lognormal`.
-        # Generate a *lognormal-distributed*, pseudo-random `Float64`.
-        def next_lognormal(loc : {{t1}}, sigma : {{t2}}) : Float64
-          Math.exp(next_normal * sigma + loc)
-        end
-      {% end %}
-    {% end %}
+      if sigma.class < Float
+        Alea.sanity_check(sigma, :sigma, :lognormal)
+      end
+
+      __next_lognormal64(next_normal * sigma.to_f64 + loc.to_f64)
+    end
+
+    # Generate a *log-normal-distributed*, pseudo-random `Float64`.
+    # Unparsed version of `#lognormal`.
+    #
+    # **@notes**:
+    # * `loc` is `0.0`.
+    # * `sigma` is `1.0`.
+    def next_lognormal : Float64
+      __next_lognormal64 next_normal
+    end
+
+    # Generate a *log-normal-distributed*, pseudo-random `Float64`.
+    # Unparsed version of `#lognormal`.
+    #
+    # **@parameters**:
+    # * `loc`: centrality parameter, or mean of the underlying normal distribution;
+    #   usually mentioned as **`μ`**.
+    #
+    # **@notes**:
+    # * `sigma` is `1.0`.
+    def next_lognormal(loc : Float64) : Float64
+      __next_lognormal64(next_normal + loc)
+    end
+
+    # Generate a *log-normal-distributed*, pseudo-random `Float64`.
+    # Unparsed version of `#lognormal`.
+    #
+    # **@parameters**:
+    # * `loc`: centrality parameter, or mean of the underlying normal distribution;
+    #   usually mentioned as **`μ`**.
+    # * `sigma`: scale parameter, or standard deviation of the underlying normal distribution;
+    #   usually mentioned as **`σ`**.
+    def next_lognormal(loc : Float64, sigma : Float64) : Float64
+      __next_lognormal64(next_normal * sigma + loc)
+    end
+
+    # Generate a *log-normal-distributed*, pseudo-random `Float64`.
+    # Unwrapped version of `lognormal`.
+    private def __next_lognormal64(norm : Float64) : Float64
+      Math.exp(norm)
+    end
   end
 end
