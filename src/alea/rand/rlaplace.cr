0e862fba81a410a3062bbfc0ac28a91701298a75
Elia Franzella <eliafranzella@live.it>
2020-06-04 17:06:33 +0200

    REF,BC: Reshaped modularity in laplace sampling methods

--- src/alea/rand/rlaplace.cr
+++ src/alea/rand/rlaplace.cr
@@ -15,16 +15,62 @@ module Alea
     # * `Alea::InfinityError` if any of the arguments is `Infinity`.
     # * `Alea::UndefinedError` if `scale` is negative or zero.
     def laplace(loc = 0.0, scale = 1.0)
-      Alea.sanity_check(loc, :loc, :laplace)
-      Alea.sanity_check(scale, :scale, :laplace)
-      Alea.param_check(scale, :<=, 0.0, :scale, :laplace)
-      next_laplace loc, scale
+      __laplace64 loc, scale
     end
 
-    # :nodoc:
-    # Unwrapped version of `laplace`.
+    # Run-time argument sanitizer for `#laplace`.
+    private def __laplace64(loc : Number, scale : Number) : Float64
+      Alea.param_check(scale, :<=, 0.0, :scale, :laplace)
+
+      if loc.class < Float
+        Alea.sanity_check(loc, :loc, :laplace)
+      end
+
+      if scale.class < Float
+        Alea.sanity_check(scale, :scale, :laplace)
+      end
+
+      __next_laplace64 * scale.to_f64 + loc.to_f64
+    end
+
     # Generate a *laplace-distributed*, pseudo-random `Float64`.
+    # Unparsed version of `laplace`.
+    #
+    # **@notes**:
+    # * `loc` is `0.0`.
+    # * `scale` is `1.0`.
     def next_laplace : Float64
+      __next_laplace64
+    end
+
+    # Generate a *laplace-distributed*, pseudo-random `Float64`.
+    # Unparsed version of `laplace`.
+    #
+    # **@parameters**:
+    # * `loc`: centrality parameter, or mean of the distribution;
+    #   usually mentioned as **`μ`**.
+    #
+    # **@notes**:
+    # * `scale` is `1.0`.
+    def next_laplace(loc : Float64) : Float64
+      __next_laplace64 + loc
+    end
+
+    # Generate a *laplace-distributed*, pseudo-random `Float64`.
+    # Unparsed version of `laplace`.
+    #
+    # **@parameters**:
+    # * `loc`: centrality parameter, or mean of the distribution;
+    #   usually mentioned as **`μ`**.
+    # * `scale`: scale parameter of the distribution;
+    #   usually mentioned as **`b`**.
+    def next_laplace(loc : Float64, scale : Float64) : Float64
+      __next_laplace64 * scale + loc
+    end
+
+    # Generate a *laplace-distributed*, pseudo-random `Float64`.
+    # Unwrapped version of `laplace`.
+    private def __next_laplace64 : Float64
       while true
         u = @prng.next_f64
         u >= 0.5 && return -Math.log(2.0 - u - u)
@@ -32,29 +78,5 @@ module Alea
         # Reject u == 0.0
       end
     end
-
-    # This are written to allow any combination of
-    # argument types and avoid tedious manual casting.
-    {% for t1 in ["Int".id, "Float".id] %}
-      # :nodoc:
-      # Unwrapped version of `laplace`.
-      # Generate a *laplace-distributed*, pseudo-random `Float64`.
-      def next_laplace(loc : {{t1}}) : Float64
-        next_laplace + loc
-      end
-    {% end %}
-
-    # This are written to allow any combination of
-    # argument types and avoid tedious manual casting.
-    {% for t1 in ["Int".id, "Float".id] %}
-      {% for t2 in ["Int".id, "Float".id] %}
-        # :nodoc:
-        # Unwrapped version of `laplace`.
-        # Generate a *laplace-distributed*, pseudo-random `Float64`.
-        def next_laplace(loc : {{t1}}, scale : {{t2}}) : Float64
-          next_laplace * scale + loc
-        end
-      {% end %}
-    {% end %}
   end
 end
