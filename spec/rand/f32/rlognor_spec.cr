56438d08f542eea32f0ff125d390bbbedf53b9e9
Elia Franzella <eliafranzella@live.it>
2020-06-05 04:46:47 +0200

    ENH,SPEC: Added 32-bit sampling support for lognormal

---
+++ spec/rand/f32/rlognor_spec.cr
@@ -0,0 +1,129 @@
+require "../../spec_helper"
+
+describe Alea do
+  context "Lognormal" do
+    describe Alea::Random do
+      describe "#lognormal32" do
+        arg_test("accepts any sized Int/UInt/Float as argument(s)",
+          caller: SpecRng,
+          method: :lognormal32,
+          params: {loc: 1.0, sigma: 1.0},
+          return_type: Float32,
+          types: [Int8, Int16, Int32, Int64, Int128,
+                  UInt8, UInt16, UInt32, UInt64, UInt128,
+                  Float32, Float64,
+          ]
+        )
+
+        sanity_test(
+          caller: SpecRng,
+          method: :lognormal32,
+          params: {loc: 1.0, sigma: 1.0},
+          params_to_check: [:loc, :sigma],
+        )
+
+        param_test(
+          caller: SpecRng,
+          method: :lognormal32,
+          params: {loc: 1.0, sigma: 1.0},
+          params_to_check: [:sigma],
+          check_negatives: true,
+          check_zeros: true,
+        )
+      end
+
+      describe "#next_lognormal32" do
+        # mean  is:   exp( m + s^2 / 2 )
+        # stdev is:   sqrt( exp(2m + s^2) * (exp(s^2) - 1) )
+
+        dist_test("generates log-normal-distributed random values with fixed loc 0.0 and sigma 0.1 parameters",
+          caller: SpecRng,
+          method: :next_lognormal32,
+          params: {loc: 0.0f32, sigma: 0.1f32},
+          sample_type: Float32,
+          real_mean: 1.005012520859401,
+          real_stdev: 0.10075302944620396,
+          mean_tol: 0.005,
+          stdev_tol: 0.007,
+        )
+
+        dist_test("generates log-normal-distributed random values with fixed loc 0.0 and sigma 0.01 parameters",
+          caller: SpecRng,
+          method: :next_lognormal32,
+          params: {loc: 0.0f32, sigma: 0.01f32},
+          sample_type: Float32,
+          real_mean: 1.0000500012500209,
+          real_stdev: 0.010000750030211357,
+          mean_tol: 0.005,
+          stdev_tol: 0.007,
+        )
+
+        dist_test("generates log-normal-distributed random values with fixed loc 0.0 and sigma 0.00001 parameters",
+          caller: SpecRng,
+          method: :next_lognormal32,
+          params: {loc: 0.0f32, sigma: 0.00001f32},
+          sample_type: Float32,
+          real_mean: 1.00000000005,
+          real_stdev: 1.0000000414201846e-5,
+          mean_tol: 0.005,
+          stdev_tol: 0.007,
+        )
+
+        dist_test("generates log-normal-distributed random values with fixed loc 0.0 and sigma 10.0 parameters",
+          pending: true,
+          caller: SpecRng,
+          method: :next_lognormal32,
+          params: {loc: 0.0f32, sigma: 10.0f32},
+          sample_type: Float32,
+          real_mean: 0.0,
+          real_stdev: 2.6881171418161356e+43,
+          mean_tol: 0.005,
+          stdev_tol: 0.007,
+        )
+
+        dist_test("generates log-normal-distributed random values with fixed loc 0.0 and sigma 1.0 parameters",
+          caller: SpecRng,
+          method: :next_lognormal32,
+          sample_type: Float32,
+          real_mean: 1.6487212707001282,
+          real_stdev: 2.1611974158950877,
+          mean_tol: 0.005,
+          stdev_tol: 0.007,
+        )
+
+        dist_test("generates log-normal-distributed random values with fixed loc 3.0 and sigma 1.0 parameters",
+          caller: SpecRng,
+          method: :next_lognormal32,
+          params: {loc: 3.0f32},
+          sample_type: Float32,
+          real_mean: 33.11545195869231,
+          real_stdev: 43.40881049525856,
+          mean_tol: 0.005,
+          stdev_tol: 0.007,
+        )
+
+        dist_test("generates log-normal-distributed random values with fixed loc 3.0 and sigma 0.5 parameters",
+          caller: SpecRng,
+          method: :next_lognormal32,
+          params: {loc: 3.0f32, sigma: 0.5f32},
+          sample_type: Float32,
+          real_mean: 22.75989509352673,
+          real_stdev: 12.129666457739873,
+          mean_tol: 0.005,
+          stdev_tol: 0.007,
+        )
+
+        dist_test("generates log-normal-distributed random values with fixed loc -3.0 and sigma 0.5 parameters",
+          caller: SpecRng,
+          method: :next_lognormal32,
+          params: {loc: -3.0f32, sigma: 0.5f32},
+          sample_type: Float32,
+          real_mean: 0.05641613950377735,
+          real_stdev: 0.03006643713435963,
+          mean_tol: 0.005,
+          stdev_tol: 0.007,
+        )
+      end
+    end
+  end
+end
--- src/alea/rand/rlognor.cr
+++ src/alea/rand/rlognor.cr
@@ -74,5 +74,77 @@ module Alea
     private def __next_lognormal64(norm : Float64) : Float64
       Math.exp(norm)
     end
+
+    # Generate a *log-normal-distributed*, pseudo-random `Float32`.
+    #
+    # **@parameters**:
+    # * `loc`: centrality parameter, or mean of the underlying normal distribution;
+    #   usually mentioned as **`μ`**.
+    # * `sigma`: scale parameter, or standard deviation of the underlying normal distribution;
+    #   usually mentioned as **`σ`**.
+    #
+    # **@exceptions**:
+    # * `Alea::NaNError` if any of the arguments is `NaN`.
+    # * `Alea::InfinityError` if any of the arguments is `Infinity`.
+    # * `Alea::UndefinedError` if `sigma` is negative or zero.
+    def lognormal32(loc = 0.0f32, sigma = 1.0f32)
+      __lognormal32 loc, sigma
+    end
+
+    # Run-time argument sanitizer for `#lognormal32`.
+    private def __lognormal32(loc : Number, sigma : Number) : Float32
+      Alea.param_check(sigma, :<=, 0.0, :sigma, :lognormal32)
+
+      if loc.class < Float
+        Alea.sanity_check(loc, :loc, :lognormal32)
+      end
+
+      if sigma.class < Float
+        Alea.sanity_check(sigma, :sigma, :lognormal32)
+      end
+
+      __next_lognormal32(next_normal32 * sigma.to_f32 + loc.to_f32)
+    end
+
+    # Generate a *log-normal-distributed*, pseudo-random `Float32`.
+    # Unparsed version of `#lognormal32`.
+    #
+    # **@notes**:
+    # * `loc` is `0.0`.
+    # * `sigma` is `1.0`.
+    def next_lognormal32 : Float32
+      __next_lognormal32 next_normal32
+    end
+
+    # Generate a *log-normal-distributed*, pseudo-random `Float32`.
+    # Unparsed version of `#lognormal32`.
+    #
+    # **@parameters**:
+    # * `loc`: centrality parameter, or mean of the underlying normal distribution;
+    #   usually mentioned as **`μ`**.
+    #
+    # **@notes**:
+    # * `sigma` is `1.0`.
+    def next_lognormal32(loc : Float32) : Float32
+      __next_lognormal32(next_normal32 + loc)
+    end
+
+    # Generate a *log-normal-distributed*, pseudo-random `Float32`.
+    # Unparsed version of `#lognormal32`.
+    #
+    # **@parameters**:
+    # * `loc`: centrality parameter, or mean of the underlying normal distribution;
+    #   usually mentioned as **`μ`**.
+    # * `sigma`: scale parameter, or standard deviation of the underlying normal distribution;
+    #   usually mentioned as **`σ`**.
+    def next_lognormal32(loc : Float32, sigma : Float32) : Float32
+      __next_lognormal32(next_normal32 * sigma + loc)
+    end
+
+    # Generate a *log-normal-distributed*, pseudo-random `Float32`.
+    # Unwrapped version of `lognormal32`.
+    private def __next_lognormal32(norm : Float32) : Float32
+      Math.exp(norm)
+    end
   end
 end
