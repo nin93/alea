20de511781ef1a6e57442601875f2ca3a6287a94
Elia Franzella <eliafranzella@live.it>
2020-06-15 20:19:14 +0200

    REF: Added `Core::SpecFun` module

---
+++ src/alea/core/cspec.cr
@@ -0,0 +1,10 @@
+module Alea::Core::SpecFun
+  # Iteration limits within which `Alea::NoConvergeError` is not raised
+  MAX_ITER = 10_000
+
+  # Single precision tolerance
+  EPSILON_32 = 1.0e-06
+
+  # Double precision tolerance
+  EPSILON_64 = 1.0e-15
+end
