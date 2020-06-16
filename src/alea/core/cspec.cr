6ecfecb6d41c1ac502a8901d8c0ad45f729273d8
Elia Franzella <eliafranzella@live.it>
2020-06-16 18:35:14 +0200

    MAINT: `Core::Gamma`: Decreased single precision tolerance

--- src/alea/core/cspec.cr
+++ src/alea/core/cspec.cr
@@ -3,7 +3,7 @@ module Alea::Core::SpecFun
   MAX_ITER = 10_000
 
   # Single precision tolerance
-  EPSILON_32 = 1.0e-06
+  EPSILON_32 = 1.0e-07
 
   # Double precision tolerance
   EPSILON_64 = 1.0e-15
